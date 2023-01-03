96152794c0cdad25d02985b0d8c313b6f2e15dbe
brettl1991 <brettl1991@gmail.com>
2023-01-03 16:59:48 +0000

    Add image to about page

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,16 +1,31 @@
 import BackLink from "~/components/BackLink";
+import nature from "./imges/nature.jpg";
 
 const About = () => {
   return (
-    <div>
+    <div className="main-page">
       <div className="nav">
         <BackLink />
       </div>
-      <h1>About Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
-        vel!
-      </p>
+      <div className="about">
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ipsum
+          quas beatae autem ad impedit. Officia, corrupti delectus? Sed
+          voluptate dolore a libero commodi molestias ducimus officiis omnis
+          eum, impedit vero aliquam nobis iure, saepe similique tempora eveniet
+          eligendi! Repellendus illo libero numquam vitae quidem soluta quam
+          neque praesentium consequatur! Corporis molestiae possimus sed
+          reiciendis quod suscipit enim vitae assumenda modi minus perferendis
+          maiores dignissimos et, explicabo harum deserunt illo quos adipisci
+          corrupti hic! Accusantium voluptatum consequatur eveniet voluptatibus
+          debitis, sit natus minima incidunt nobis doloremque vitae officiis
+          minus ab deserunt rem reiciendis laudantium tempora consequuntur,
+          earum fugit perferendis! Laudantium.
+        </p>
+
+        <img className="about-img" src={nature} alt="nature" />
+      </div>
     </div>
   );
 };
