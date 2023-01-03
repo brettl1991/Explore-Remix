d33af9446b0b772e3c3afe44a91bd41e5be0cc56
brettl1991 <brettl1991@gmail.com>
2023-01-03 16:57:49 +0000

    Creating new contacts page

---
+++ app/routes/contacts/contacts.tsx
@@ -0,0 +1,18 @@
+import BackLink from "~/components/BackLink";
+
+const Contacts = () => {
+  return (
+    <div className="main-page">
+      <div className="nav">
+        <BackLink />
+      </div>
+      <h1>Contacts</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque magnam
+        omnis molestias ab ipsa? Blanditiis quaerat deleniti atque odio porro.
+      </p>
+    </div>
+  );
+};
+
+export default Contacts;
