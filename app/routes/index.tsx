f4ea09a9cc25379966c0e9e9c32d7ec2d183d46e
brettl1991 <brettl1991@gmail.com>
2023-01-03 16:58:29 +0000

    Add page sections to the main page website

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,7 +2,11 @@ import { Link } from "@remix-run/react";
 
 const Main = () => {
   return (
-    <div>
+    <div className="main-page">
+      <div className="header">
+        <h1>Home Page</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+      </div>
       <div className="nav">
         <Link className="link" to="/about">
           About
@@ -10,10 +14,34 @@ const Main = () => {
         <Link className="link" to="/services/services">
           Services
         </Link>
+        <Link className="link" to="/contacts/contacts">
+          Contacts
+        </Link>
       </div>
-      <div className="home-pg">
-        <h1>Home Page</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+      <div className="row">
+        <div className="side">
+          <h2>About Me</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            eveniet mollitia veritatis architecto est ex. Iure, qui voluptatum?
+            Dicta aut ad aspernatur? Sunt inventore voluptate facere dicta at
+            sint nemo. Mollitia iste minus veritatis quae sunt fuga! Omnis, amet
+            nisi!
+          </p>
+        </div>
+
+        <div className="main">
+          <h2>Work With Me</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur quaerat tenetur id eligendi molestias corrupti! Non,
+            numquam maxime doloribus iste aliquam nemo, dicta corporis adipisci
+            vel repellendus quasi. Quos, voluptate.
+          </p>
+        </div>
+      </div>
+      <div className="footer">
+        <h2>Footer</h2>
       </div>
     </div>
   );
